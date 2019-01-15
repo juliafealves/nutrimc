fcf8a8c0579d16d4928dbe14439b94cd338e4892
juliafealves <juliafealves>
2019-01-14 23:29:58 -0300

    Calculated IMC to first patient.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-var title = document.querySelector('.title');
+const title = document.querySelector('.title');
 title.textContent = 'NutrIMC';
+
+const patient = document.querySelector('#first-patient');
+const weight = patient.querySelector('.info-peso').textContent;
+const height = patient.querySelector('.info-altura').textContent;
+const imc = weight / (height *  height);
+
+patient.querySelector('.info-imc').textContent = imc;
