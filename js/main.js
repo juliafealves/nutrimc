58efeff1ed94ed0121f7139dda3749eb3ac57965
juliafealves <juliafealves>
2019-01-14 23:55:59 -0300

    Validated data weight and height.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ title.textContent = 'NutrIMC';
 const patient = document.querySelector('#first-patient');
 const weight = patient.querySelector('.info-peso').textContent;
 const height = patient.querySelector('.info-altura').textContent;
-const imc = weight / (height *  height);
 
-patient.querySelector('.info-imc').textContent = imc;
+const tdIMC = patient.querySelector('.info-imc');
+
+isValidWeight = true;
+isValidHeight = true;
+
+if (weight <= 0 || weight >= 1000) {
+    tdIMC.textContent = 'Weight is not valid!';
+    isValidWeight = false;
+}
+
+if (height <= 0 || height >= 3) {
+    tdIMC.textContent = 'Height is not valid!';
+    isValidHeight = false;
+}
+
+if (isValidHeight && isValidWeight) {
+    tdIMC.textContent = weight / (height * height);
+}
